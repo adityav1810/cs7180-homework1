290d2dabf41176f33267343b49b60e75ea1db3b4
Luv Verma <luvverma2011@gmail.com>
2023-09-21 23:25:35 -0400

    Update losses.py

--- losses.py
+++ losses.py
@@ -6,74 +6,70 @@ Authors : Luv Verma and Aditya Varshney
 This file contains the loss functions for the model
 '''
 
-
 import torch
 import torch.nn as nn
 from torchvision import transforms
 
 
-class MeanShift(nn.Conv2d):
+class RGBMeanShift(nn.Conv2d):
     '''
     Compute Mean Shift of a Conv2D layer
-
     '''
     def __init__(
-        self, rgb_range = 1,
-        norm_mean=(0.485, 0.456, 0.406), norm_std=(0.229, 0.224, 0.225), sign=-1):
+        self, rgb_range_val=1,
+        normalization_mean=(0.485, 0.456, 0.406), normalization_std=(0.229, 0.224, 0.225), operation_sign=-1):
 
-        super(MeanShift, self).__init__(3, 3, kernel_size=1)
-        std = torch.Tensor(norm_std)
-        self.weight.data = torch.eye(3).view(3, 3, 1, 1) / std.view(3, 1, 1, 1)
-        self.bias.data = sign * rgb_range * torch.Tensor(norm_mean) / std
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        super(RGBMeanShift, self).__init__(3, 3, kernel_size=1)
+        std_tensor = torch.Tensor(normalization_std)
+        self.weight.data = torch.eye(3).view(3, 3, 1, 1) / std_tensor.view(3, 1, 1, 1)
+        self.bias.data = operation_sign * rgb_range_val * torch.Tensor(normalization_mean) / std_tensor
+        self.device_instance = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         
-        for p in self.parameters():
-            p.requires_grad = False
+        for param in self.parameters():
+            param.requires_grad = False
 
 
-class perceptual_loss(nn.Module):
+class VGGPerceptualLoss(nn.Module):
     '''
-    Compute Perceptual loss from vgg pretrained model
+    Compute Perceptual loss from VGG pretrained model
     '''
-
-    def __init__(self, vgg):
-        super(perceptual_loss, self).__init__()
-        self.normalization_mean = [0.485, 0.456, 0.406]
-        self.normalization_std = [0.229, 0.224, 0.225]
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.transform = MeanShift(norm_mean = self.normalization_mean, norm_std = self.normalization_std).to(self.device)
-        self.vgg = vgg
-        self.criterion = nn.MSELoss()
-    def forward(self, HR, SR, layer = 'relu5_4'):
+    def __init__(self, vgg_model):
+        super(VGGPerceptualLoss, self).__init__()
+        self.norm_mean_values = [0.485, 0.456, 0.406]
+        self.norm_std_values = [0.229, 0.224, 0.225]
+        self.device_instance = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.mean_shift = RGBMeanShift(norm_mean=self.norm_mean_values, norm_std=self.norm_std_values).to(self.device_instance)
+        self.vgg_instance = vgg_model
+        self.loss_criterion = nn.MSELoss()
+    def forward(self, hr_image, sr_image, layer_name='relu5_4'):
         ## HR and SR should be normalized [0,1]
-        hr = self.transform(HR)
-        sr = self.transform(SR)
+        normalized_hr = self.mean_shift(hr_image)
+        normalized_sr = self.mean_shift(sr_image)
         
-        hr_feat = getattr(self.vgg(hr), layer)
-        sr_feat = getattr(self.vgg(sr), layer)
+        hr_feature = getattr(self.vgg_instance(normalized_hr), layer_name)
+        sr_feature = getattr(self.vgg_instance(normalized_sr), layer_name)
         
-        return self.criterion(hr_feat, sr_feat), hr_feat, sr_feat
+        return self.loss_criterion(hr_feature, sr_feature), hr_feature, sr_feature
 
-class TVLoss(nn.Module):
+class TotalVariationLoss(nn.Module):
     '''
     Compute Total Variance Loss
     '''
-    def __init__(self, tv_loss_weight=1):
-        super(TVLoss, self).__init__()
-        self.tv_loss_weight = tv_loss_weight
+    def __init__(self, tv_weight=1):
+        super(TotalVariationLoss, self).__init__()
+        self.tv_weight_value = tv_weight
 
-    def forward(self, x):
-        batch_size = x.size()[0]
-        h_x = x.size()[2]
-        w_x = x.size()[3]
-        count_h = self.tensor_size(x[:, :, 1:, :])
-        count_w = self.tensor_size(x[:, :, :, 1:])
-        h_tv = torch.pow((x[:, :, 1:, :] - x[:, :, :h_x - 1, :]), 2).sum()
-        w_tv = torch.pow((x[:, :, :, 1:] - x[:, :, :, :w_x - 1]), 2).sum()
+    def forward(self, image_tensor):
+        batch_count = image_tensor.size()[0]
+        height = image_tensor.size()[2]
+        width = image_tensor.size()[3]
+        height_count = self.tensor_dimension(image_tensor[:, :, 1:, :])
+        width_count = self.tensor_dimension(image_tensor[:, :, :, 1:])
+        height_variation = torch.pow((image_tensor[:, :, 1:, :] - image_tensor[:, :, :height - 1, :]), 2).sum()
+        width_variation = torch.pow((image_tensor[:, :, :, 1:] - image_tensor[:, :, :, :width - 1]), 2).sum()
         
-        return self.tv_loss_weight * 2 * (h_tv / count_h + w_tv / count_w) / batch_size
+        return self.tv_weight_value * 2 * (height_variation / height_count + width_variation / width_count) / batch_count
 
     @staticmethod
-    def tensor_size(t):
-        return t.size()[1] * t.size()[2] * t.size()[3]
-
+    def tensor_dimension(tensor_obj):
+        return tensor_obj.size()[1] * tensor_obj.size()[2] * tensor_obj.size()[3]
