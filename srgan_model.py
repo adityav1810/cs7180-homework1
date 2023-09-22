ca522d02832d16ca27447fbae99022bc9a8acf6b
Luv Verma <luvverma2011@gmail.com>
2023-09-21 23:31:04 -0400

    Update srgan_model.py

--- srgan_model.py
+++ srgan_model.py
@@ -10,76 +10,77 @@ import torch
 import torch.nn as nn
 from ops import *
 
-
-class Generator(nn.Module):
+class SuperResGenerator(nn.Module):
     '''
     Define the Generator Network for the SRGAN
     '''
-
-    
-    def __init__(self, img_feat = 3, n_feats = 64, kernel_size = 3, num_block = 16, act = nn.PReLU(), scale=4):
-        super(Generator, self).__init__()
-        # first layer
+    def __init__(self, input_channels=3, feature_channels=64, filter_size=3, num_res_blocks=16, activation_fn=nn.PReLU(), upscale_factor=4):
+        super(SuperResGenerator, self).__init__()
+        # Initial convolutional layer
+        self.initial_conv = ConvolutionBlock(input_channels, feature_channels, filter_size=9, activation_fn=activation_fn)
         
-        self.conv01 = conv(in_channel = img_feat, out_channel = n_feats, kernel_size = 9, BN = False, act = act)
-        # RDBs
+        # Residual blocks
+        residual_blocks = [ResidualBlock(feature_channels, filter_size, activation_fn) for _ in range(num_res_blocks)]
+        self.res_blocks = nn.Sequential(*residual_blocks)
         
-        resblocks = [ResBlock(channels = n_feats, kernel_size = 3, act = act) for _ in range(num_block)]
-        self.body = nn.Sequential(*resblocks)
-        # conv layer
-        self.conv02 = conv(in_channel = n_feats, out_channel = n_feats, kernel_size = 3, BN = True, act = None)
-        # upsampling blocks
+        # Middle convolutional layer
+        self.middle_conv = ConvolutionBlock(feature_channels, feature_channels, filter_size, use_bn=True)
         
-        if(scale == 4):
-            upsample_blocks = [Upsampler(channel = n_feats, kernel_size = 3, scale = 2, act = act) for _ in range(2)]
+        # Upsampling blocks
+        if upscale_factor == 4:
+            upsample_layers = [UpscaleBlock(feature_channels, filter_size, scale=2, activation_fn=activation_fn) for _ in range(2)]
         else:
-            upsample_blocks = [Upsampler(channel = n_feats, kernel_size = 3, scale = scale, act = act)]
-        # top layers
-        self.tail = nn.Sequential(*upsample_blocks)
+            upsample_layers = [UpscaleBlock(feature_channels, filter_size, scale=upscale_factor, activation_fn=activation_fn)]
+            
+        self.upsample_seq = nn.Sequential(*upsample_layers)
         
-        self.last_conv = conv(in_channel = n_feats, out_channel = img_feat, kernel_size = 3, BN = False, act = nn.Tanh())
+        # Final convolutional layer
+        self.final_conv = ConvolutionBlock(feature_channels, input_channels, filter_size, activation_fn=nn.Tanh())
         
-    def forward(self, x):
+    def forward(self, tensor_input):
+        tensor_input = self.initial_conv(tensor_input)
+        skip_tensor = tensor_input
         
-        x = self.conv01(x)
-        _skip_connection = x
+        tensor_input = self.res_blocks(tensor_input)
+        tensor_input = self.middle_conv(tensor_input)
+        # Add a skip connection
+        features = tensor_input + skip_tensor
         
-        x = self.body(x)
-        x = self.conv02(x)
-        # add a skip connection
-        feat = x + _skip_connection
+        tensor_input = self.upsample_seq(features)
+        tensor_input = self.final_conv(tensor_input)
         
-        x = self.tail(feat)
-        x = self.last_conv(x)
-        
-        return x, feat
+        return tensor_input, features
     
-class Discriminator(nn.Module):
+class SuperResDiscriminator(nn.Module):
     '''
     Define the Discriminator Network for the SRGAN 
-
     '''
-    
-    def __init__(self, img_feat = 3, n_feats = 64, kernel_size = 3, act = nn.LeakyReLU(inplace = True), num_of_block = 3, patch_size = 96):
-        super(Discriminator, self).__init__()
-        self.act = act
-        self.conv01 = conv(in_channel = img_feat, out_channel = n_feats, kernel_size = 3, BN = False, act = self.act)
-        self.conv02 = conv(in_channel = n_feats, out_channel = n_feats, kernel_size = 3, BN = False, act = self.act, stride = 2)
-        body = [discrim_block(in_feats = n_feats * (2 ** i), out_feats = n_feats * (2 ** (i + 1)), kernel_size = 3, act = self.act) for i in range(num_of_block)]    
-        self.body = nn.Sequential(*body)
-        self.linear_size = ((patch_size // (2 ** (num_of_block + 1))) ** 2) * (n_feats * (2 ** num_of_block))
-        tail = []
-        tail.append(nn.Linear(self.linear_size, 1024))
-        tail.append(self.act)
-        tail.append(nn.Linear(1024, 1))
-        tail.append(nn.Sigmoid())
-        self.tail = nn.Sequential(*tail)
+    def __init__(self, input_channels=3, feature_channels=64, filter_size=3, activation_fn=nn.LeakyReLU(inplace=True), num_disc_blocks=3, patch_dim=96):
+        super(SuperResDiscriminator, self).__init__()
+        self.activation_fn = activation_fn
         
+        # Initial layers
+        self.start_conv1 = ConvolutionBlock(input_channels, feature_channels, filter_size, activation_fn=self.activation_fn)
+        self.start_conv2 = ConvolutionBlock(feature_channels, feature_channels, filter_size, stride=2, activation_fn=self.activation_fn)
         
-    def forward(self, x):        
-        x = self.conv02(x)
-        x = self.body(x)        
-        x = x.view(-1, self.linear_size)
-        x = self.tail(x)
-        return x
-
+        # Discriminator blocks
+        disc_blocks = [DiscriminatorBlock(feature_channels * (2 ** i), feature_channels * (2 ** (i + 1)), filter_size, activation_fn=self.activation_fn) for i in range(num_disc_blocks)]
+        self.disc_seq = nn.Sequential(*disc_blocks)
+        
+        # Linear layers
+        self.linear_dim = ((patch_dim // (2 ** (num_disc_blocks + 1))) ** 2) * (feature_channels * (2 ** num_disc_blocks))
+        
+        end_layers = [
+            nn.Linear(self.linear_dim, 1024),
+            self.activation_fn,
+            nn.Linear(1024, 1),
+            nn.Sigmoid()
+        ]
+        self.end_seq = nn.Sequential(*end_layers)
+        
+    def forward(self, tensor_input):        
+        tensor_input = self.start_conv2(tensor_input)
+        tensor_input = self.disc_seq(tensor_input)        
+        tensor_input = tensor_input.view(-1, self.linear_dim)
+        tensor_input = self.end_seq(tensor_input)
+        return tensor_input
