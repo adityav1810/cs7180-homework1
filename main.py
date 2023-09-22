f26823731aa8085646f351530bda32bd77270c03
Luv Verma <luvverma2011@gmail.com>
2023-09-21 23:41:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,46 +3,43 @@ CS 7180 : Advanced Perception
 Homework 1
 Authors : Luv Verma and Aditya Varshney
 
-This file contains the main arg parser which stimulates the model training, testing and validation tasks
-
+This file contains the main argument parser which stimulates the model training, testing, and validation tasks.
 '''
 
-from mode import *
+from operation_mode import *
 import argparse
 
+arg_parser = argparse.ArgumentParser()
 
-parser = argparse.ArgumentParser()
+def string_to_boolean(value):
+    return value.lower() in ('true')
 
-def str2bool(v):
-    return v.lower() in ('true')
+arg_parser.add_argument("--LR_path", type=str, default='../dataSet/DIV2K/DIV2K_valid_LR_bicubic/X4')
+arg_parser.add_argument("--GT_path", type=str, default='../dataSet/DIV2K/DIV2K_train_HR/')
+arg_parser.add_argument("--res_num", type=int, default=16)
+arg_parser.add_argument("--num_workers", type=int, default=0)
+arg_parser.add_argument("--batch_size", type=int, default=16)
+arg_parser.add_argument("--L2_coeff", type=float, default=1.0)
+arg_parser.add_argument("--adv_coeff", type=float, default=1e-3)
+arg_parser.add_argument("--tv_loss_coeff", type=float, default=0.0)
+arg_parser.add_argument("--pre_train_epoch", type=int, default=8000)
+arg_parser.add_argument("--fine_train_epoch", type=int, default=4000)
+arg_parser.add_argument("--scale", type=int, default=4)
+arg_parser.add_argument("--patch_size", type=int, default=24)
+arg_parser.add_argument("--feat_layer", type=str, default='relu5_4')
+arg_parser.add_argument("--vgg_rescale_coeff", type=float, default=0.006)
+arg_parser.add_argument("--fine_tuning", type=string_to_boolean, default=False)
+arg_parser.add_argument("--in_memory", type=string_to_boolean, default=True)
+arg_parser.add_argument("--generator_path", type=str)
+arg_parser.add_argument("--operation_mode", type=str, default='train')
 
-parser.add_argument("--LR_path", type = str, default = '../dataSet/DIV2K/DIV2K_valid_LR_bicubic/X4')
-parser.add_argument("--GT_path", type = str, default = '../dataSet/DIV2K/DIV2K_train_HR/')
-parser.add_argument("--res_num", type = int, default = 16)
-parser.add_argument("--num_workers", type = int, default = 0)
-parser.add_argument("--batch_size", type = int, default = 16)
-parser.add_argument("--L2_coeff", type = float, default = 1.0)
-parser.add_argument("--adv_coeff", type = float, default = 1e-3)
-parser.add_argument("--tv_loss_coeff", type = float, default = 0.0)
-parser.add_argument("--pre_train_epoch", type = int, default = 8000)
-parser.add_argument("--fine_train_epoch", type = int, default = 4000)
-parser.add_argument("--scale", type = int, default = 4)
-parser.add_argument("--patch_size", type = int, default = 24)
-parser.add_argument("--feat_layer", type = str, default = 'relu5_4')
-parser.add_argument("--vgg_rescale_coeff", type = float, default = 0.006)
-parser.add_argument("--fine_tuning", type = str2bool, default = False)
-parser.add_argument("--in_memory", type = str2bool, default = True)
-parser.add_argument("--generator_path", type = str)
-parser.add_argument("--mode", type = str, default = 'train')
+parsed_args = arg_parser.parse_args()
 
-args = parser.parse_args()
-
-if args.mode == 'train':
-    train(args)
+if parsed_args.operation_mode == 'train':
+    train(parsed_args)
     
-elif args.mode == 'test':
-    test(args)
+elif parsed_args.operation_mode == 'test':
+    test(parsed_args)
     
-elif args.mode == 'validate':
-    validate(args)
-
+elif parsed_args.operation_mode == 'validate':
+    validate(parsed_args)
